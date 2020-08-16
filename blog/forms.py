ede84836e6f7302a2465503f2aa621fefbb6bc76
Sergo Ostroushchenko <ostroushchenko.sa@gmail.com>
2020-08-16 09:36:11 +0300

    refactor forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -4,10 +4,10 @@ from blog.models import Post
 
 
 class PostCreateForm(forms.ModelForm):
-
     class Meta:
         model = Post
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Введите текст', 'class': "form-control"}),
+            'title': forms.TextInput(
+                attrs={'placeholder': 'Введите текст', 'class': "form-control"}),
         }
         fields = ['title', 'about', 'image']
